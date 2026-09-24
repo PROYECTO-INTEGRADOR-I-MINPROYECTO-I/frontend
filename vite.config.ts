88935ef2b0548edc95c99ed7f55caa2657855c54
gamberfox <gamberfox@gmail.com>
2026-09-24 14:06:42 -0500

    setting up the QA libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
@@ -7,7 +7,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react(),
     tailwindcss()
-  ],
+  ]as any,
   resolve: {
     alias: {
       react: path.resolve(__dirname, './node_modules/react'),
@@ -17,5 +17,9 @@ export default defineConfig({
   },
   server: {
     proxy: { "/api": { target: "http://localhost:8000", changeOrigin: true } },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
   },
 })
